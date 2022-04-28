3a001b93e160a58ddaceaf8a85171f27138bc566
VuksanFilip <filipvuksan.iphone@gmail.com>
2022-04-28 19:41:36 +0200

    feat: adding exceptions

--- Hospital/Users/UserRepository.cs
+++ Hospital/Users/UserRepository.cs
@@ -1,4 +1,14 @@
 using MongoDB.Driver;
+[System.Serializable]
+public class UserDoesNotExistException : System.Exception
+{
+    public UserDoesNotExistException() { }
+    public UserDoesNotExistException(string message) : base(message) { }
+    public UserDoesNotExistException(string message, System.Exception inner) : base(message, inner) { }
+    protected UserDoesNotExistException(
+        System.Runtime.Serialization.SerializationInfo info,
+        System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+}
 
 namespace Hospital
 {
