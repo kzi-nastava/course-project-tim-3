c809d605a7c79c6aa0bd95f238cf62334801b786
Katarina Mirkovic <mirkovicka01@gmail.com>
2022-04-28 17:32:21 +0200

    Added ObjectId generator

--- Hospital/People/Person.cs
+++ Hospital/People/Person.cs
@@ -1,14 +1,20 @@
 using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
 
 namespace Hospital
 {
-    [BsonKnownTypes(typeof(Patient))]
+    [BsonKnownTypes(typeof(Patient), typeof(Doctor))]
+    
     public abstract class Person
     {
-        public string FirstName {get;}
-        public string LastName {get;}
+        [BsonId]
+        public ObjectId Id { get; set; }
+        private int myVar;
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
 
         public Person(string firstName, string lastName) {
+            Id = ObjectId.GenerateNewId();
             FirstName = firstName;
             LastName = lastName;
         }
