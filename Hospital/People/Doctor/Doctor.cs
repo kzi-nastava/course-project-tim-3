13d60b7155b219d9d5d4ec2e167b42d5c1a34250
Katarina Mirkovic <mirkovicka01@gmail.com>
2022-04-28 17:45:33 +0200

    Changed doctor specialty access modifier

--- Hospital/People/Doctor/Doctor.cs
+++ Hospital/People/Doctor/Doctor.cs
@@ -2,11 +2,12 @@ namespace Hospital
 {
     public class Doctor : Person
     {
-        private String Specialty {get; set;}
+        public String Specialty {get; set;}
         
         public Doctor(string firstName, string lastName, string specialty) : base(firstName, lastName)
         {
             Specialty = specialty;
         }
     }
+
 }
