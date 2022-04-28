6621335219db350c8c4f69415b02c93fb541a4f2
Katarina Mirkovic <mirkovicka01@gmail.com>
2022-04-28 17:15:19 +0200

    Defined Doctor attributes and constructors, added repository methods

--- Hospital/People/Doctor/Doctor.cs
+++ Hospital/People/Doctor/Doctor.cs
@@ -2,7 +2,8 @@ namespace Hospital
 {
     public class Doctor : Person
     {
-        private String Specialty {get;}
+        private String Specialty {get; set;}
+        
         public Doctor(string firstName, string lastName, string specialty) : base(firstName, lastName)
         {
             Specialty = specialty;
---
+++ Hospital/People/Doctor/DoctorRepository.cs
@@ -0,0 +1,26 @@
+using MongoDB.Driver;
+
+namespace Hospital
+{
+    public class DoctorRepository
+    {
+        private MongoClient _dbClient;
+
+        public DoctorRepository(MongoClient _dbClient)
+        {
+            this._dbClient = _dbClient;
+        }
+
+        public IMongoCollection<Doctor> GetDoctors()
+        {
+            return _dbClient.GetDatabase("hospital").GetCollection<Doctor>("doctors");
+        }
+
+        public void AddDoctor(string firstName, string lastName, string specialty)
+        {
+            var newDoctor = new Doctor(firstName, lastName, specialty);
+            var doctors = GetDoctors();
+            doctors.ReplaceOne(user => user.Email == newUser.Email, newDoctor, new ReplaceOptions {IsUpsert = true});
+        }
+    }
+}
