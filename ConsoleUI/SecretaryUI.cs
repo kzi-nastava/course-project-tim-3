02ae1ceead939f80b21496a0b7902bc6c3274977
VuksanFilip <filipvuksan.iphone@gmail.com>
2022-04-28 21:35:19 +0200

    feat: Implemented commands list

--- ConsoleUI/SecretaryUI.cs
+++ ConsoleUI/SecretaryUI.cs
@@ -7,5 +7,7 @@ public class SecretaryUI : ConsoleUI
         this._user = _user;
     }
 
+    public List<string> Commands {get; private set;} = new List<string> {"Create", "Read", "Update", "Delete"};
+
      public override void Start(){}
 }
