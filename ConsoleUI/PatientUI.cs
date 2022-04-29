6d5ede4a2676c14074ae031c46749d8856f1a2f0
VladimirGolosin <vladimirgolosin1@gmail.com>
2022-04-29 17:44:20 +0200

    feat: Change function selectDate to list time slots for appointments instead of free input

--- ConsoleUI/PatientUI.cs
+++ ConsoleUI/PatientUI.cs
@@ -14,7 +14,7 @@ public class NullInputException : System.Exception
 public class PatientUI : ConsoleUI
 {
     public List<string> MainCommands {get; private set;} = new List<string> {"ma / manage appointments","exit","help"};
-    public List<string> ManageAppointmentsCommands {get; private set;} = new List<string> 
+    public List<string> ManageAppointmentsCommands {get; private set;} = new List<string>
     {"ca / create appointment",
     "ua / update appointment",
     "va / view appointments",
@@ -23,47 +23,96 @@ public class PatientUI : ConsoleUI
     "exit",
     "help"};
 
+    //there might be a better way to set opening time, only time will be used
+    //those times should be stored somewhere else
+    DateTime openingTime = new DateTime(2000, 10, 20, 9, 0, 0);
+    DateTime closingTime = new DateTime(2000, 10, 20, 17, 0, 0);
+
+    
+
     public PatientUI(Hospital _hospital, User? _user) : base(_hospital) 
     {
         this._user = _user;
     }
 
-    DateTime? inputDate ()
-    {
+    DateTime? selectDate ()
+    {   
+        DateTime result = new DateTime();
+
+        // date selection
+
         Console.Write("Please enter a date in dd-MM-yyyy format: ");
-        string? date = Console.ReadLine();
-        if (date is null)
+        string? inputDate = Console.ReadLine();
+        if (inputDate is null)
         {
             throw new NullInputException("Null value as input");
         }
 
-        date = date.Trim();
+        inputDate = inputDate.Trim();
 
-        Console.Write("Please enter a time in HH:mm format :");
-        string? time = Console.ReadLine();
-        if (time is null)
-        {
-            throw new NullInputException("Null value as input");
-        }
-
-        time = time.Trim();
-        DateTime selectedDateTime;
-        
-        bool success = DateTime.TryParseExact(date+" "+time, 
-                       "dd-MM-yyyy HH:mm", 
+        bool success = DateTime.TryParseExact(inputDate, 
+                       "dd-MM-yyyy", 
                        CultureInfo.InvariantCulture, 
                        DateTimeStyles.None, 
-                       out selectedDateTime);
-        
-        if (success) 
-            return selectedDateTime;
-        else
+                       out result);
+
+        if (!success) 
+        {
+            Console.WriteLine("Error - wrong date. Aborting...");    
             return null;
+        }
         
+        // time selection
+
+        int appointmentIndex = 0;
+        TimeSpan appointmentDuration = new TimeSpan(0,0,15,0);
+        DateTime iterationTime = openingTime;
+        
+        while (iterationTime.TimeOfDay != closingTime.TimeOfDay)
+        {
+            Console.WriteLine(appointmentIndex + " - " + iterationTime.ToString("HH:mm"));
+            iterationTime = iterationTime.Add(appointmentDuration);
+            appointmentIndex += 1;
+        }
+
+        //while loop will add a sufficient "1" at the end of the loop
+        appointmentIndex -= 1;
+
+        Console.Write("Please enter a number from the list: ");
+        string? input = Console.ReadLine();
+        if (input is null)
+        {
+            throw new NullInputException("Null value as input");
+        }
+
+        int selectedIndex;
+        try
+        {
+            selectedIndex = Int32.Parse(input);
+            Console.WriteLine(selectedIndex);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error - wrong input. Aborting...");
+            return null;
+        }
+
+        result = result.AddHours(9);
+        if (selectedIndex >= 0 && selectedIndex <= appointmentIndex)
+        {
+            result = result.Add(selectedIndex*appointmentDuration);
+        }
+        else
+        {
+            Console.WriteLine("Error - wrong number. Aborting...");
+        }
+
+        return result;
     }
+
     public void createAppointment()
     {
-        DateTime? selectedDate = inputDate();
+        DateTime? selectedDate = selectDate();
         Console.WriteLine(selectedDate);
     }
     public string selectOption(string commandGroup="")
