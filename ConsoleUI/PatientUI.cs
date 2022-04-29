8010d22e7bef18a2f06854302951a14b40ca2c40
VladimirGolosin <vladimirgolosin1@gmail.com>
2022-04-29 15:12:12 +0200

    feat: Add manage appointments menu and dateTime selection function

--- ConsoleUI/PatientUI.cs
+++ ConsoleUI/PatientUI.cs
@@ -1,4 +1,5 @@
 namespace Hospital;
+using System.Globalization;
 
 [System.Serializable]
 public class NullInputException : System.Exception
@@ -12,15 +13,65 @@ public class NullInputException : System.Exception
 }
 public class PatientUI : ConsoleUI
 {
+    public List<string> MainCommands {get; private set;} = new List<string> {"ma / manage appointments","exit","help"};
+    public List<string> ManageAppointmentsCommands {get; private set;} = new List<string> 
+    {"ca / create appointment",
+    "ua / update appointment",
+    "va / view appointments",
+    "da / delete appointment",
+    "return",
+    "exit",
+    "help"};
+
     public PatientUI(Hospital _hospital, User? _user) : base(_hospital) 
     {
         this._user = _user;
-        
     }
 
-    public string selectOption()
+    DateTime? inputDate ()
     {
+        Console.Write("Please enter a date in dd-MM-yyyy format: ");
+        string? date = Console.ReadLine();
+        if (date is null)
+        {
+            throw new NullInputException("Null value as input");
+        }
+
+        date = date.Trim();
+
+        Console.Write("Please enter a time in HH:mm format :");
+        string? time = Console.ReadLine();
+        if (time is null)
+        {
+            throw new NullInputException("Null value as input");
+        }
+
+        time = time.Trim();
+        DateTime selectedDateTime;
         
+        bool success = DateTime.TryParseExact(date+" "+time, 
+                       "dd-MM-yyyy HH:mm", 
+                       CultureInfo.InvariantCulture, 
+                       DateTimeStyles.None, 
+                       out selectedDateTime);
+        
+        if (success) 
+            return selectedDateTime;
+        else
+            return null;
+        
+    }
+    public void createAppointment()
+    {
+        DateTime? selectedDate = inputDate();
+        Console.WriteLine(selectedDate);
+    }
+    public string selectOption(string commandGroup="")
+    {
+        if (commandGroup != "")
+        {
+            Console.Write("["+commandGroup+"] ");
+        }
         Console.Write("Please enter a command: ");
         string? input = Console.ReadLine();
         if (input is null)
@@ -31,17 +82,56 @@ public class PatientUI : ConsoleUI
 
     }
 
-    public List<string> Commands {get; private set;} = new List<string> {"ma / manage appointments","exit","help"};
-
     public void manageAppointments()
     {
-        Console.WriteLine("wip");
+        Console.Clear();
+        System.Console.WriteLine("");
+
+        printCommands(this.ManageAppointmentsCommands);
+        while (true){
+            string selectedOption = selectOption("Manage appointments");
+            if (selectedOption == "ca" || selectedOption == "create appointment")
+            {
+                createAppointment();
+            }
+            else if (selectedOption == "ua" || selectedOption == "update appointment")
+            {
+                Console.WriteLine("wip2");
+            }
+            else if (selectedOption == "va" || selectedOption == "view appointments")
+            {
+                Console.WriteLine("wip3");
+            }
+            else if (selectedOption == "da" || selectedOption == "delete appointment")
+            {
+                Console.WriteLine("wip4");
+            }
+            else if (selectedOption == "help")
+            {
+                printCommands(this.ManageAppointmentsCommands);
+            }
+            else if (selectedOption == "return")
+            {
+                Console.WriteLine("Returning...");
+                Console.WriteLine("");
+                break;
+            }
+            else if (selectedOption == "exit")
+            {
+                Console.WriteLine("Exiting...");
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Unrecognized command, please try again");
+            }
+        }
     }
 
-    public void printCommands()
+    public void printCommands(List<string> commands)
     {
         Console.WriteLine("Available commands: ");
-        foreach (string command in this.Commands)
+        foreach (string command in commands)
         {
             Console.WriteLine(command);
         }
@@ -52,7 +142,7 @@ public class PatientUI : ConsoleUI
         Console.Clear();
         System.Console.WriteLine("");
 
-        printCommands();
+        printCommands(this.MainCommands);
         while (true){
             string selectedOption = selectOption();
             if (selectedOption == "ma" || selectedOption == "manage appointments")
@@ -61,7 +151,7 @@ public class PatientUI : ConsoleUI
             }
             else if (selectedOption == "help")
             {
-                printCommands();
+                printCommands(this.MainCommands);
             }
             else if (selectedOption == "exit")
             {
