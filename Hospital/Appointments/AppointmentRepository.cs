a0fb3265dbc588cc1857f336b4bd59e5f04d6240
Katarina Mirkovic <mirkovicka01@gmail.com>
2022-04-29 23:58:03 +0200

    Added AppointmentRepository methods for getting an appointment by given attribute

--- Hospital/Appointments/AppointmentRepository.cs
+++ Hospital/Appointments/AppointmentRepository.cs
@@ -1,19 +1,61 @@
 using MongoDB.Driver;
+using MongoDB.Bson;
 
 namespace Hospital
 {
     public class AppointmentRepository
     {
         private MongoClient _dbClient;
-
         public AppointmentRepository(MongoClient _dbClient)
         {
             this._dbClient = _dbClient;
         }
+        
+        // public IMongoCollection<Appointment> GetAppointments()
+        // {
+        //     return _dbClient.GetDatabase("hospital").GetCollection<Appointment>("appointments");
+        // }
 
-        public IMongoCollection<Appointment> GetAppointments()
+        public IMongoCollection<Checkup> GetCheckups()
         {
-            return _dbClient.GetDatabase("hospital").GetCollection<Appointment>("appointments");
+            return _dbClient.GetDatabase("hospital").GetCollection<Checkup>("checkups");
         }
+
+        public IMongoCollection<Operation> GetOperations()
+        {
+            return _dbClient.GetDatabase("hospital").GetCollection<Operation>("operations");
+        }
+
+        public void AddCheckup(DateTime timeAndDate, MongoDBRef patient, MongoDBRef doctor, TimeSpan duration, string anamnesis)
+        {
+            var newCheckup = new Checkup(timeAndDate, patient, doctor, anamnesis);
+            var checkups = GetCheckups();
+            var apCheck =  newCheckup;
+            checkups.ReplaceOne(appointment => appointment.Id == apCheck.Id, apCheck, new ReplaceOptions {IsUpsert = true});
+        }
+
+        public void AddOperation(DateTime timeAndDate, MongoDBRef patient, MongoDBRef doctor, TimeSpan duration, string report)
+        {
+            var newOperation = new Operation(timeAndDate, patient, doctor, report);
+            var operations = GetOperations();
+            var apCheck = newOperation;
+            operations.ReplaceOne(appointment => appointment.Id == apCheck.Id, apCheck, new ReplaceOptions {IsUpsert = true});
+        }
+
+        public List<Checkup> GetCheckupsByDoctor(ObjectId id)
+        {
+            var checkups = GetCheckups();
+            List<Checkup> doctorsCheckups = checkups.Find(appointment => appointment.Doctor.Id == id).ToList();
+            return doctorsCheckups;
+        }
+        
+        public List<Operation> GetOperationsByDoctor(ObjectId id)
+        {
+            var operations = GetOperations();
+            List<Operation> doctorsOperations = operations.Find(appointment => appointment.Doctor.Id == id).ToList();
+            return doctorsOperations;
+        }
+
+        
     }
 }
