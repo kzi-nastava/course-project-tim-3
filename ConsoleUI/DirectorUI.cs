34be17e44efca7b6660ca48ddaf23d842f260677
swopl <modaik5@gmail.com>
2022-04-30 20:23:56 +0200

    fix(dir): unhandled bad parsing of enum

--- ConsoleUI/DirectorUI.cs
+++ ConsoleUI/DirectorUI.cs
@@ -38,11 +38,10 @@ namespace Hospital
             while (true)
             {
                 System.Console.Clear();
+                DisplayRooms(_hospital.RoomRepo);
                 System.Console.WriteLine(@"
-                --- MANAGE ROOMS ---
                 INPUT OPTION:
                     [add room|add|ar|a] Add a room
-                    [read|r] Room rooms
                     [quit|q] Quit to main menu
                     [exit|x] Exit program
                     [...]
@@ -67,34 +66,31 @@ namespace Hospital
 
                         System.Console.Write("ENTER ROOM TYPE [rest|operation|examination|other] >> ");
                         var rawType = ReadSanitizedLine();
-                        Enum.TryParse(rawType, true, out Room.RoomType type);
+                        bool success = Enum.TryParse(rawType, true, out Room.RoomType type);
+                        if (!success)
+                            throw new InvalidInputException("NOT A VALID TYPE!");
 
                         var newRoom = new Room(location, name, type);
                         _hospital.RoomRepo.AddRoom(newRoom);
                         System.Console.Write("SUCCESSFULLY ADDED ROOM. INPUT ANYTHING TO CONTINUE >> ");
                         ReadSanitizedLine();
                     }
-                    else if (choice == "read" || choice == "r")
-                    {
-
-                    }
                     else if (choice == "q" || choice == "quit")
                         throw new QuitToMainMenuException("From StartManageRooms");
                     else if (choice == "x" || choice == "exit")
                         System.Environment.Exit(0);
                 }
-                catch (ArgumentException)
-                {
-                    System.Console.Write("NOT A VALID TYPE! INPUT ANYTHING TO CONTINUE >> ");
-                    ReadSanitizedLine();
-                }
                 catch (InvalidInputException e)
                 {
-                    System.Console.Write(e.Message + "INPUT ANYTHING TO CONTINUE >> ");
+                    System.Console.Write(e.Message + " INPUT ANYTHING TO CONTINUE >> ");
                     ReadSanitizedLine();
                 }
             }
+        }
 
+        public void DisplayRooms(RoomRepository roomRepo)
+        {
+            
         }
     }
 }
