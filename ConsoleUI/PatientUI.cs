a8313ebc4c8c25779c720a7519b057cb67cc57a2
Katarina Mirkovic <mirkovicka01@gmail.com>
2022-04-30 23:47:29 +0200

    Resolve conflicts after merge

--- ConsoleUI/PatientUI.cs
+++ ConsoleUI/PatientUI.cs
@@ -1,17 +1,8 @@
 namespace Hospital;
 using System.Globalization;
 using MongoDB.Bson;
+using MongoDB.Driver;
 
-[System.Serializable]
-public class NullInputException : System.Exception
-{
-    public NullInputException() { }
-    public NullInputException(string message) : base(message) { }
-    public NullInputException(string message, System.Exception inner) : base(message, inner) { }
-    protected NullInputException(
-        System.Runtime.Serialization.SerializationInfo info,
-        System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
-}
 
 [System.Serializable]
 public class GetOutException : System.Exception
@@ -51,7 +42,7 @@ public class PatientUI : ConsoleUI
     public PatientUI(Hospital _hospital, User? _user) : base(_hospital) 
     {
         this._user = _user;
-        current = _hospital.PatientRepo.GetPatientByName(_user.Person.FirstName);
+        current = _hospital.PatientRepo.GetPatientById((ObjectId)_user.Person.Id);
     }
 
     public void UpdateCheckup(){
@@ -498,7 +489,7 @@ public class PatientUI : ConsoleUI
         //public Checkup(DateTime timeAndDate, MongoDBRef patient, MongoDBRef doctor, string anamnesis)
         Checkup newCheckup = new Checkup(
             (DateTime)selectedDate,
-            new MongoDB.Driver.MongoDBRef("patients", ((Patient)_user.Person).Id),
+            new MongoDB.Driver.MongoDBRef("patients", (_user.Person).Id),
             new MongoDB.Driver.MongoDBRef("doctors", suitableDoctors[selectedIndex].Id),
             "no anamnesis");
         
