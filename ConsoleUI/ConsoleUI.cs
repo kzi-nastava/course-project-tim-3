3424fc8118734a2313df3fb54bda32d8cccc430d
swopl <modaik5@gmail.com>
2022-04-30 19:15:06 +0200

    feat: add helpful exception and reading utils to CUI

--- ConsoleUI/ConsoleUI.cs
+++ ConsoleUI/ConsoleUI.cs
@@ -1,4 +1,16 @@
 namespace Hospital;
+
+[System.Serializable]
+public class QuitToMainMenuException : System.Exception
+{
+    public QuitToMainMenuException() { }
+    public QuitToMainMenuException(string message) : base(message) { }
+    public QuitToMainMenuException(string message, System.Exception inner) : base(message, inner) { }
+    protected QuitToMainMenuException(
+        System.Runtime.Serialization.SerializationInfo info,
+        System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+}
+
 public abstract class ConsoleUI
 {
     protected Hospital _hospital;
@@ -9,5 +21,16 @@ public abstract class ConsoleUI
     public ConsoleUI(Hospital hospital)
     {
         this._hospital = hospital;
+    }
+
+    public string ReadSanitizedLine()
+    {
+        var raw = System.Console.ReadLine();
+        string sanitized;
+        if (raw is null)
+            sanitized = "";
+        else
+            sanitized = raw.ToLower();
+        return sanitized;
     }
 }
