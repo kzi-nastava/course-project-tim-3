046494ac6fdda1e0836fc6574113b5fecfae4f11
VladimirGolosin <vladimirgolosin1@gmail.com>
2022-05-08 19:45:21 +0200

    feat: add list of checkup change logs as patient atribute

--- Hospital/People/Patient/Patient.cs
+++ Hospital/People/Patient/Patient.cs
@@ -5,11 +5,21 @@ namespace Hospital
     {
         [BsonElement]
         public MedicalRecord MedicalRecord {get; set;}
+        public List<CheckupChangeLog> CheckupChangeLogs {get; set;}
         public Patient(string firstName, string lastName, MedicalRecord medicalRecord) : base(firstName, lastName)
         {
             FirstName = firstName;
             LastName = lastName;
             MedicalRecord = medicalRecord;
+            CheckupChangeLogs = new List<CheckupChangeLog>();
+        }
+
+        public Patient(string firstName, string lastName, MedicalRecord medicalRecord, List<CheckupChangeLog> checkupChangeLogs) : base(firstName, lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            MedicalRecord = medicalRecord;
+            CheckupChangeLogs = checkupChangeLogs;
         }
 
         public override string ToString()
