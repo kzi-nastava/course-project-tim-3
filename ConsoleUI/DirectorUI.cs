722b961d5c97ad7165d8f2d564ffb241a2f5e143
swopl <modaik5@gmail.com>
2022-05-08 19:11:44 +0200

    feat(dir): implement log out

--- ConsoleUI/DirectorUI.cs
+++ ConsoleUI/DirectorUI.cs
@@ -21,6 +21,7 @@ public class DirectorUI : ConsoleUI
                 INPUT OPTION:
                     [manage rooms|mr] Manage rooms and stockrooms
                     [manage equipment|me] Manage equipment
+                    [log out|lo] Log out
                     [exit|x] Exit program
                 ");  // TODO: add the rest of the features
                 System.Console.Write(">> ");
@@ -29,6 +30,8 @@ public class DirectorUI : ConsoleUI
                     StartManageRooms();
                 else if (choice == "me" || choice == "manage equipment")
                     StartManageEquipment();
+                else if (choice == "lo" || choice == "log out")
+                    return;
                 else if (choice == "x" || choice == "exit")
                     System.Environment.Exit(0);
             }
