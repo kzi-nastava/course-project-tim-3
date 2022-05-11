d1a270893515e6ee573a9349f4cfe214a29d673a
swopl <modaik5@gmail.com>
2022-05-11 17:05:13 +0200

    refactor(dir): equip UI split func

--- ConsoleUI/Director/EquipmentUI.cs
+++ ConsoleUI/Director/EquipmentUI.cs
@@ -5,20 +5,21 @@ namespace Hospital;
 
 public class EquipmentUI : ConsoleUI
 {
+    private List<EquipmentBatch> _loadedBatches;
+
     public EquipmentUI(Hospital hospital) : base(hospital)
     {
-
+        _loadedBatches = _hospital.EquipmentRepo.GetAll().ToList();
     }
 
     public override void Start()
     {
         // TODO: load schedules
-        List<EquipmentBatch> equipmentBatches = _hospital.EquipmentRepo.GetAll().ToList();
         while (true)
         {
             System.Console.Clear();
             System.Console.WriteLine("--- EQUIPMENTS ---");
-            DisplayEquipmentBatches(equipmentBatches);
+            DisplayEquipmentBatches(_loadedBatches);
             System.Console.WriteLine(@"
             INPUT OPTION:
                 [search equipment|search|se] Search equipment batches
@@ -33,50 +34,20 @@ public class EquipmentUI : ConsoleUI
                 // todo: unhardcode choices so they match menu display always
                 if (choice == "se" || choice == "search" || choice == "search equipment")
                 {
-                    System.Console.WriteLine("Example filters: min:3 max:100 type:checkup");
-                    System.Console.WriteLine("You can leave out any that you want to keep at any value. Range is inclusive");
-                    System.Console.WriteLine("Available types: checkup, operation, furniture, hallway");
-
-                    System.Console.Write("INPUT YOUR FILTERS >> ");
-                    var filters = ReadSanitizedLine().Trim();
-                    var query = new EquipmentQuery(filters);
-
-                    System.Console.Write("INPUT YOUR SEARCH TERM >> ");
-                    var search = ReadSanitizedLine();
-                    query.NameContains = new Regex(search);
-
-                    equipmentBatches = _hospital.EquipmentRepo.Search(query).ToList();
+                    Search();
                 }
                 else if (choice == "me" || choice == "move" || choice == "move equipment")
                 {
-                    // TODO: functionalize
-                    System.Console.Write("SELECT EQUIP TO MOVE >> ");
-                    int index = ReadInt(0, equipmentBatches.Count - 1);
-                    var equipmentBatch = equipmentBatches[index];
-
-                    System.Console.Write("SELECT AMOUNT TO MOVE (MINIMUM 1. AVAILABLE: " + equipmentBatch.Count + ") >> ");
-                    int amount = ReadInt(1, equipmentBatch.Count);
-
-                    System.Console.Write("INPUT DATE-TIME WHEN IT IS DONE >> ");
-                    var rawDate = ReadSanitizedLine();
-                    var whenDone = DateTime.Parse(rawDate);
-
-                    List<Room> rooms = _hospital.RoomRepo.GetAll().ToList();
-                    RoomUI.DisplayRooms(rooms);
-                    System.Console.Write("INPUT ROOM NUMBER >> ");
-                    var number = ReadInt(0, rooms.Count - 1);
-                    
-                    var relocation = new EquipmentRelocation(equipmentBatch.Name, amount, 
-                        equipmentBatch.Type, whenDone, (ObjectId) equipmentBatch.Room.Id, rooms[number].Id);
-                    _hospital.RelocationRepo.Add(relocation);
-                    _hospital.RelocationRepo.Schedule(relocation);
-                    System.Console.Write("RELOCATION SCHEDULED SUCCESSFULLY. INPUT ANYTHING TO CONTINUE >> ");
-                    ReadSanitizedLine();
+                    Move();
                 }
                 else if (choice == "q" || choice == "quit")
+                {
                     throw new QuitToMainMenuException("From StartManageEquipments");
+                }
                 else if (choice == "x" || choice == "exit")
+                {
                     System.Environment.Exit(0);
+                }
                 else
                 {
                     System.Console.WriteLine("INVALID INPUT - READ THE AVAILABLE COMMANDS!");
@@ -109,5 +80,48 @@ public class EquipmentUI : ConsoleUI
             System.Console.WriteLine(i + " | " + room?.Location + " | " + equipmentBatch.Type + 
                                         " | " + equipmentBatch.Name + " | " + equipmentBatch.Count);
         }
+    }
+
+    private void Move()
+    {
+        System.Console.Write("SELECT EQUIP TO MOVE >> ");
+        int index = ReadInt(0, _loadedBatches.Count - 1);
+        var equipmentBatch = _loadedBatches[index];
+
+        System.Console.Write("SELECT AMOUNT TO MOVE (MINIMUM 1. AVAILABLE: " + equipmentBatch.Count + ") >> ");
+        int amount = ReadInt(1, equipmentBatch.Count);
+
+        System.Console.Write("INPUT DATE-TIME WHEN IT IS DONE >> ");
+        var rawDate = ReadSanitizedLine();
+        var whenDone = DateTime.Parse(rawDate);
+
+        List<Room> rooms = _hospital.RoomRepo.GetAll().ToList();
+        RoomUI.DisplayRooms(rooms);
+        System.Console.Write("INPUT ROOM NUMBER >> ");
+        var number = ReadInt(0, rooms.Count - 1);
+        
+        var relocation = new EquipmentRelocation(equipmentBatch.Name, amount, 
+            equipmentBatch.Type, whenDone, (ObjectId) equipmentBatch.Room.Id, rooms[number].Id);
+        _hospital.RelocationRepo.Add(relocation);
+        _hospital.RelocationRepo.Schedule(relocation);
+        System.Console.Write("RELOCATION SCHEDULED SUCCESSFULLY. INPUT ANYTHING TO CONTINUE >> ");
+        ReadSanitizedLine();
+    }
+
+    private void Search()
+    {
+        System.Console.WriteLine("Example filters: min:3 max:100 type:checkup");
+        System.Console.WriteLine("You can leave out any that you want to keep at any value. Range is inclusive");
+        System.Console.WriteLine("Available types: checkup, operation, furniture, hallway");
+
+        System.Console.Write("INPUT YOUR FILTERS >> ");
+        var filters = ReadSanitizedLine().Trim();
+        var query = new EquipmentQuery(filters);
+
+        System.Console.Write("INPUT YOUR SEARCH TERM >> ");
+        var search = ReadSanitizedLine();
+        query.NameContains = new Regex(search);
+
+        _loadedBatches = _hospital.EquipmentRepo.Search(query).ToList();
     }
 }
