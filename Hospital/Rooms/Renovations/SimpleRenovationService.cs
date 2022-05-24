b241b8843f9120f1be9132a918c7d29b214c4bcd
swopl <modaik5@gmail.com>
2022-05-24 16:47:00 +0200

    fix: trying to insert already existing when doing ScheduleAll

--- Hospital/Rooms/Renovations/SimpleRenovationService.cs
+++ Hospital/Rooms/Renovations/SimpleRenovationService.cs
@@ -4,19 +4,23 @@ namespace HospitalSystem;
 
 public class SimpleRenovationService
 {
-    private ISimpleRenovationRepository _simpleRenovationRepo;
+    private ISimpleRenovationRepository _repo;
     private RoomService _roomService;
 
-    public SimpleRenovationService(ISimpleRenovationRepository simpleRenovationRepo, RoomService roomService)
+    public SimpleRenovationService(ISimpleRenovationRepository repo, RoomService roomService)
     {
-        _simpleRenovationRepo = simpleRenovationRepo;
+        _repo = repo;
         _roomService = roomService;
     }
 
     public void Schedule(SimpleRenovation renovation)
     {
-        _simpleRenovationRepo.Insert(renovation);
+        _repo.Insert(renovation);
+        JustSchedule(renovation);
+    }
 
+    private void JustSchedule(SimpleRenovation renovation)
+    {
         Scheduler.Schedule(renovation.BusyRange.Starts, () =>
         {
             _roomService.Deactivate(renovation.RoomLocation);
@@ -30,17 +34,17 @@ public class SimpleRenovationService
     private void FinishRenovation(SimpleRenovation renovation)
     {
         renovation.IsDone = true;
-        _simpleRenovationRepo.Replace(renovation);
+        _repo.Replace(renovation);
         _roomService.Activate(renovation.RoomLocation);
     }
 
     public void ScheduleAll()
     {
-        foreach (var renovation in _simpleRenovationRepo.GetAll())
+        foreach (var renovation in _repo.GetAll())
         {
             if (!renovation.IsDone)
             {
-                Schedule(renovation);
+                JustSchedule(renovation);
             }
         }
     }
