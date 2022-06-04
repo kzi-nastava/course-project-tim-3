3c06e4837e59127ce08cb330da1afdfd00ebd2c0
VladimirGolosin <vladimirgolosin1@gmail.com>
2022-06-04 15:32:18 +0200

    fix: fix pr errors

--- HospitalSystem.Core/People/Doctor/DoctorService.cs
+++ HospitalSystem.Core/People/Doctor/DoctorService.cs
@@ -29,36 +29,36 @@ public class DoctorService
     }
 
     public List<Doctor> GetManyBySpecialty(string keyword)
-        {
-            var doctors = GetAll();
-            var allDoctors =
-                from doctor in doctors.AsQueryable<Doctor>()
-                select doctor;
+    {
+        var doctors = GetAll();
+        var allDoctors =
+            from doctor in doctors.AsQueryable<Doctor>()
+            select doctor;
 
-            List<Doctor> filteredDoctors = allDoctors.ToList().FindAll(doctor => doctor.Specialty.ToString().Contains(keyword.ToUpper()));
-            return filteredDoctors;
-        }
+        List<Doctor> filteredDoctors = allDoctors.ToList().FindAll(doctor => doctor.Specialty.ToString().Contains(keyword.ToUpper()));
+        return filteredDoctors;
+    }
 
-        public List<Doctor> GetManyByName(string keyword)
-        {
-            var doctors = GetAll();
-            var allDoctors =
-                from doctor in doctors.AsQueryable<Doctor>()
-                select doctor;
+    public List<Doctor> GetManyByName(string keyword)
+    {
+        var doctors = GetAll();
+        var allDoctors =
+            from doctor in doctors.AsQueryable<Doctor>()
+            select doctor;
 
-            return allDoctors.ToList().FindAll(doctor => doctor.FirstName.Contains(keyword));
-        }
+        return allDoctors.ToList().FindAll(doctor => doctor.FirstName.Contains(keyword));
+    }
 
-         public List<Doctor> GetManyByLastName(string keyword)
-        {
-            var doctors = GetAll();
-            var allDoctors =
-                from doctor in doctors.AsQueryable<Doctor>()
-                select doctor;
+    public List<Doctor> GetManyByLastName(string keyword)
+    {
+        var doctors = GetAll();
+        var allDoctors =
+            from doctor in doctors.AsQueryable<Doctor>()
+            select doctor;
 
-            List<Doctor> filteredDoctors = allDoctors.ToList().FindAll(doctor => doctor.LastName.Contains(keyword));
-            return filteredDoctors;
-        }
+        List<Doctor> filteredDoctors = allDoctors.ToList().FindAll(doctor => doctor.LastName.Contains(keyword));
+        return filteredDoctors;
+    }
 
     public Doctor GetOneBySpecialty(Specialty specialty)
     {
