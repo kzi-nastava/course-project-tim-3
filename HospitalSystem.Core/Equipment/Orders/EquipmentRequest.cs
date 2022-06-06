30f4e30afbe1b6f5b4d7e673d1f31286b27f3d37
VuksanFilip <filipvuksan.iphone@gmail.com>
2022-06-06 15:27:54 +0200

    feat: Created EquipmentRequest.cs

---
+++ HospitalSystem.Core/Equipment/Orders/EquipmentRequest.cs
@@ -0,0 +1,29 @@
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace HospitalSystem.Core;
+
+public class EquipmentRequest
+{
+    [BsonId]
+    public ObjectId Id { get; set; }
+    public string ToStockLocation { get; set; }
+    public string Name { get; set; }
+    public EquipmentType Type { get; set; }
+    public int Count { get; set; }
+    [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
+    public DateTime EndTime { get; set; }
+    public bool IsDone { get; set; }
+
+    public EquipmentRequest(string name, int count, EquipmentType type, 
+        DateTime endTime, string toStockLocation)
+    {
+        Id = ObjectId.GenerateNewId();
+        ToStockLocation = toStockLocation;
+        Name = name;
+        Count = count;
+        EndTime = endTime;
+        Type = type;
+        IsDone = false;
+    }
+}
